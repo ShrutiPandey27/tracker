8055465fc3253e561de01e92e4b26b5443af6226
shruti <shrutipandey27122001@gmail.com>
2020-09-23 14:44:09 +0530

    Updates

---
+++ precache-manifest.150caa128b3c3b64d6ba50215e42b596.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca8b3ac89dde4a86c8dffacb92f7420",
+    "url": "/tracker/index.html"
+  },
+  {
+    "revision": "dca794afb497e6387fe8",
+    "url": "/tracker/static/css/main.6054f0f2.chunk.css"
+  },
+  {
+    "revision": "ad7bf0e24837f0a43ec7",
+    "url": "/tracker/static/js/2.76682bdc.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/tracker/static/js/2.76682bdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dca794afb497e6387fe8",
+    "url": "/tracker/static/js/main.8f8d1793.chunk.js"
+  },
+  {
+    "revision": "d841adee469ad2f531d7",
+    "url": "/tracker/static/js/runtime-main.66521e7b.js"
+  },
+  {
+    "revision": "9faca7c76663905da7f66409bf1bda05",
+    "url": "/tracker/static/media/corona.9faca7c7.jpg"
+  },
+  {
+    "revision": "8c0e7c46052bc2223651d55a158fee97",
+    "url": "/tracker/static/media/loremipsum.8c0e7c46.png"
+  }
+]);
